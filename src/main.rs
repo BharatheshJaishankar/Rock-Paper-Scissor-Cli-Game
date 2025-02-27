f1a9fd8164f462861c1c36282887115aa6e080db
Bharathesh Jaishankar <71770977+BharatheshJaishankar@users.noreply.github.com>
2025-02-27 13:43:03 +0000

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use dialoguer;
+use rand::prelude::IndexedRandom;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::rng();
+    loop {
+        println!("Rock, Paper, Scissors!");
+        let options = vec!["Rock", "Paper", "Scissor"];
+        let selection = dialoguer::Select::new()
+            .with_prompt("Choose your weapon")
+            .items(&options)
+            .default(0)
+            .interact()
+            .unwrap();
+        let input = options[selection];
+        let decision = options.choose(&mut rng).unwrap();
+        println!("The computer chose {}", decision);
+        if input == "Rock" && *decision == "Scissor" {
+            println!("You win!");
+            break;
+        } else {
+            if input == "Rock" && *decision == "Rock" {
+                println!("Its a tie!\nTry again\n");
+                continue;
+            } else {
+                if input == "Paper" && *decision == "Rock" {
+                    println!("You win!");
+                    break;
+                } else {
+                    if input == "Paper" && *decision == "Paper" {
+                        println!("Its a tie!\nTry again\n");
+                        continue;
+                    } else {
+                        if input == "Scissor" && *decision == "Paper" {
+                            println!("You win!");
+                            break;
+                        } else {
+                            if input == "Scissor" && *decision == "Scissor" {
+                                println!("Its a tie!\nTry again\n");
+                                continue;
+                            } else {
+                                println!("You lose!");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
